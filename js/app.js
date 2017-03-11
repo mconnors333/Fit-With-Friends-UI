3ddf065f46e5512dac04bc0cc2d18b410dc632fd
mconnors333 <matthew.connors333@gmail.com>
2017-03-11 13:05:00 -0500

    added firebase

---
+++ js/app.js
@@ -0,0 +1,12 @@
+(function() {
+    // Initialize Firebase
+    var config = {
+      apiKey: "AIzaSyC37MdWztXHcvohVxgptCuxF-CuQ895d-k",
+      authDomain: "fitwithfriends-6973a.firebaseapp.com",
+      databaseURL: "https://fitwithfriends-6973a.firebaseio.com",
+      storageBucket: "fitwithfriends-6973a.appspot.com",
+      messagingSenderId: "230459414493"
+    };
+    firebase.initializeApp(config);
+
+}());
